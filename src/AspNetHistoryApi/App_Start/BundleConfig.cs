01018c91479a6d7b1f5d9b1a3657b56c4395aea6
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-07-07 23:29:58 +0300

    Added ajax transitions between pages

--- src/AspNetHistoryApi/App_Start/BundleConfig.cs
+++ src/AspNetHistoryApi/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace AspNetHistoryApi
 {
@@ -8,8 +7,9 @@ namespace AspNetHistoryApi
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- src/AspNetHistoryApi/Controllers/HomeController.cs
+++ src/AspNetHistoryApi/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace AspNetHistoryApi.Controllers
 {
@@ -10,21 +6,21 @@ namespace AspNetHistoryApi.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return Request.IsAjaxRequest() ? (ActionResult)PartialView() : View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return Request.IsAjaxRequest() ? (ActionResult)PartialView() : View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return Request.IsAjaxRequest() ? (ActionResult)PartialView() : View();
         }
     }
 }
