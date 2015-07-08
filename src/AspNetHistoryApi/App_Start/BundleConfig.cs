a8fdf868c6012ff912782e4bba0ec5fe72954c13
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-07-09 01:45:29 +0300

    Moved tabSwitcher to a separate file

--- src/AspNetHistoryApi/App_Start/BundleConfig.cs
+++ src/AspNetHistoryApi/App_Start/BundleConfig.cs
@@ -7,9 +7,9 @@ namespace AspNetHistoryApi
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery")
-                .Include("~/Scripts/jquery-{version}.js")
-                .Include("~/Scripts/jquery.unobtrusive-ajax.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,6 +22,10 @@ namespace AspNetHistoryApi
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/common").Include(
+                      "~/Scripts/underscore.js",
+                      "~/Scripts/tabSwitcher.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ src/AspNetHistoryApi/Scripts/tabSwitcher.js
@@ -0,0 +1,55 @@
+﻿window.tabSwitcher = (function () {
+    var popstateInProgress,
+        options = {};
+
+    function init(opts) {
+        _.extend(options, opts);
+        validateOptions(options);
+
+        $(window).on('popstate', function (e) {
+            var state = e.originalEvent.state;
+            if (state && state.role === 'tab') {
+                popstateInProgress = true;
+
+                $('#' + state.linkId)
+                    .focus()
+                    .trigger('click');
+            }
+        });
+    };
+
+    function validateOptions(opts) {
+        if (!opts.titleSelector)
+            throw new Error("Please provide a value for 'titleSelector'");
+    };
+
+    function navigationSucceeded(tab) {
+        var $tab = $(tab),
+            $title = $(options.titleSelector),
+            tabHref = $tab.attr('href');
+
+        if ($title.length) {
+            document.title = $title.val();
+        }
+
+        if (!popstateInProgress && document.location.pathname !== tabHref) {
+            history.pushState({ role: 'tab', linkId: $tab.attr('id') }, null, tabHref);
+        }
+    };
+
+    function navigationFailed(tab) {
+        $(tab).blur();
+        alert('Sorry, an error occured!');
+    };
+
+    function navigationCompleted() {
+        popstateInProgress = false;
+    };
+
+    return {
+        init: init,
+        navigationSucceeded: navigationSucceeded,
+        navigationFailed: navigationFailed,
+        navigationCompleted: navigationCompleted
+    };
+})(jQuery);
