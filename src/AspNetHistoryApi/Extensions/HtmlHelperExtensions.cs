fab7c92683f538a1e30f00cd8a68a3e6824618a8
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-07-09 00:40:37 +0300

    Polished behaviour

---
+++ src/AspNetHistoryApi/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetHistoryApi.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString PartialViewTitle(this HtmlHelper html, string title)
+        {
+            var tag = new TagBuilder("input");
+            tag.MergeAttribute("id", "partialViewTitle");
+            tag.MergeAttribute("type", "hidden");
+            tag.MergeAttribute("value", title);
+
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
